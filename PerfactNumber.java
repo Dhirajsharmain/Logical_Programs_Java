dc134433525f94cefdac5ba31688b6b685fdc130
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-04 21:10:07 +0530

    Fibonacci series Program

--- PerfactNumber.java
+++ PerfactNumber.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class PerfactNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a Perfect number : ");
+        System.out.print("Enter a number : ");
         int number = scanner.nextInt();
         int sum = 0;
         for (int i = 1; i < number; i++) {
